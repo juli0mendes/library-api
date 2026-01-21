13e35f8b9e27e5c4990ef814e7e1334547990bf0
juli0mendes <jmendes@live.com>
2026-01-20 22:00:47 -0300

    feat: add AuthorRepository interface for JPA

---
+++ src/main/java/io/github/juli0mendes/library/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package io.github.juli0mendes.library.repository;
+
+import io.github.juli0mendes.library.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+}
